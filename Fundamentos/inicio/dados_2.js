37e2cfa67af5b36e7cbb527b2ff1233bf9ebb1e9
Diogo Pincetta <diogohe08@gmail.com>
2025-10-14 22:12:27 -0300

    Primeiro commit

---
+++ Fundamentos/inicio/dados_2.js
@@ -0,0 +1,17 @@
+console.log(19.90 * 0.6);
+
+
+let preco = 19.90;
+let desconto = 0.4;
+let precoComDesconto = preco * (1 - desconto)
+console.log(precoComDesconto);
+
+let nome = "Caderno"; // String (Texto) -> sequencia de símbolos
+let categoria = "Papelaria";
+console.log("Prduto: " + nome 
+    + ", Categoria: " +  categoria
+    + ", Preço: " + preco
+    + ", Desconto: " + desconto);
+
+console.log(1 + 1);
+console.log("1" + 1); // Ele irá juntar os dois valores pois não é valor númerico
